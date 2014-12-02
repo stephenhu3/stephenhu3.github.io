93058fb43d27293d3b926e8703a67636ec3996c9
stephenhu3 <stephenhu3@gmail.com>
2014-12-01 23:38:53 -0800

    theming redux, new fonts, positioning changed, revised content, fixed masonry issue

---
+++ js/masonry.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+	// Stick with this lower overhead method
+	var container = document.querySelector('#masonry');
+	var msnry;
+	// initialize Masonry after all images have loaded
+	imagesLoaded( container, function() {
+	  	var msnry = new Masonry( container, {
+		  // options
+
+		  itemSelector: '.col-xs-6.col-sm-6.col-md-6.col-lg-6'
+		});
+	});
+
+
+
+	// Using using the below code will reload masonry after all the images have loaded
+	// this adds an additional animation at the cost of performance
+
+	// var container = document.querySelector('#container');
+
+	// var msnry = new Masonry( container, {
+	//   // options
+	//    itemSelector: '.col-xs-6.col-sm-6.col-md-6.col-lg-6'
+	// });
+
+	// imagesLoaded( container, function() {
+	//   msnry.layout();
+	// });
+
+
+
+});
